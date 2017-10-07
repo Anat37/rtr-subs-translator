6288233b96e17d81ccd6c7619c640555e4e9d790
anatoly kozlov <kozlof9@yandex.ru>
2017-10-07 23:09:07 +0300

    мб добавил кнопку

--- MadDogsInDaPlace/app/src/main/java/com/abbyy/maddogs/maddogsindaplace/Dictionary.java
+++ MadDogsInDaPlace/app/src/main/java/com/abbyy/maddogs/maddogsindaplace/Dictionary.java
@@ -1,7 +1,11 @@
 package com.abbyy.maddogs.maddogsindaplace;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,7 +21,14 @@ public class Dictionary extends AppCompatActivity {
             add(new Word("Hello"));
             add(new Word("Good buy"));
         }};
-
+        Button fab = (Button) findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dictionary.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
         WordAdapter itemsAdapter = new WordAdapter(this, words);
         ListView listView = (ListView) findViewById(R.id.dictionary);
         listView.setAdapter(itemsAdapter);
